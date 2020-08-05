77352dcfc60230c68bdf10ac10f82366aa076712
avielman <avielman@gmail.com>
2020-08-05 00:12:45 -0600

    Proyecto de control de Inventarios, Trunk.

---
+++ Inventario.COMMON/Entidades/Vale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Inventario.COMMON.Entidades
+{
+    public class Vale : Base
+    {        
+        public DateTime FechaHoraSolicitud { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public DateTime? FechaEntregaReal { get; set; }
+        public List<Material> MaterialesPrestados { get; set; }
+        public Empleado Solicitante { get; set; }
+        public Empleado EncargadoDeAlmacen { get; set; }
+
+
+    }
+}
